d4e740118416db48205acc685b23728ac16616de
unknown <Admin@DELLLAP251.VITUNIVERSITY.LOCAL>
2025-12-10 11:26:11 +0530

    Ver 1

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c, sum;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b); // fetch input
+
+    sum = a + b;
+    c=sum+a;
+    printf("Sum = %d\n", sum);
+    printf("C=%d",c);a
+
+    return 0;
+}
